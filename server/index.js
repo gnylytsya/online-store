83530fb924e1c3f4aee94dafd4b3f01112cec4a1
Vitaliy Gnylytsya <gnylytsya@gmail.com>
2021-05-22 02:42:09 +0300

    config port

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
-    const express = require('express')
-    const PORT = process.env.PORT || 5000
+require('dotenv').config()
+const express = require('express')
+const PORT = process.env.PORT || 5000
 
-    const app = express()
+const app = express()
 
-    app.listen(PORT, () => console.log(`Server started on port ${5000}`))
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
