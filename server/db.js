aa9c851c91ab181f8e58a3a08cd61722adb23e66
Vitaliy Gnylytsya <gnylytsya@gmail.com>
2021-05-28 02:00:15 +0300

    added  db.js

--- server/db.js
+++ server/db.js
@@ -1,5 +1,11 @@
 const {Sequelize} = require('sequelize')
 
-module.export = new Sequelize ({
-    
+module.export = new Sequelize (
+    process.env.DB.NAME, // Name DB
+    process.env.DB.user,// User
+    process.env.DB.PASSWORD, // Password
+    {
+        dialect: 'postgres',
+        host: process.env.DB_HOST
+    }
 })
